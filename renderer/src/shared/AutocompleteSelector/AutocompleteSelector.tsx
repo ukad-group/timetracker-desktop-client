cd4ec74c3920d702c70895ad14551cb8a4704cce
d.mashchenko <dmytro.mashchenko@gmail.com>
2026-09-03 09:56:15 +0200

    ISSUE: typescript error

--- renderer/src/shared/AutocompleteSelector/AutocompleteSelector.tsx
+++ renderer/src/shared/AutocompleteSelector/AutocompleteSelector.tsx
@@ -161,10 +161,7 @@ const AutocompleteSelector = ({
           tabIndex={tabIndex}
           onBlur={handleOnBlur}
         />
-        <ComboboxButton
-          tabIndex={-1}
-          className="absolute inset-y-0 right-0 flex items-center px-2 rounded-r-md focus:outline-none"
-        >
+        <ComboboxButton className="absolute inset-y-0 right-0 flex items-center px-2 rounded-r-md focus:outline-none">
           <ChevronUpDownIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
         </ComboboxButton>
 
